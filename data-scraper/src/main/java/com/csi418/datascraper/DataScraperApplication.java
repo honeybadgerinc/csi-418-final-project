1f79255c36b719409c315836a7ae256f305a5448
honeybadgerinc <luke.henry617@gmail.com>
2018-10-08 18:57:38 -0400

    project structure created

---
+++ data-scraper/src/main/java/com/csi418/datascraper/DataScraperApplication.java
@@ -0,0 +1,12 @@
+package com.csi418.datascraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataScraperApplication.class, args);
+    }
+}
