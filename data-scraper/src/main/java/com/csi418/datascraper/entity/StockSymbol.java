18d9f18731eef6004b27e00b162fd900753378df
honeybadgerinc <luke.henry617@gmail.com>
2018-10-17 15:55:59 -0400

    reverted to initial commit

--- data-scraper/src/main/java/com/csi418/datascraper/entity/StockSymbol.java
+++
@@ -1,7 +0,0 @@
-package com.csi418.datascraper.entity;
-
-import javax.persistence.Entity;
-
-@Entity
-public class StockSymbol {
-}
--- data-scraper/src/main/java/com/csi418/datascraper/repository/StockSymbolRepository.java
+++
@@ -1,6 +0,0 @@
-package com.csi418.datascraper.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockSymbolRepository extends JpaRepository {
-}
