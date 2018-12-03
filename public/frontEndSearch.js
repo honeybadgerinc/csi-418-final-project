3b3a9aa0ca4edaeb572479b11b46e8098d3eaa09
Malcolm James <mtj416@gmail.com>
2018-12-03 18:36:34 -0500

    Hope This Works :)

--- public/frontEndSearch.js
+++ public/frontEndSearch.js
@@ -6,12 +6,16 @@ window.onload = function () {
 function scraper() {
     const Http = new XMLHttpRequest();
     const url='https://finalproject-37ce0.firebaseapp.com/scrape';
-    Http.open("GET", url);
-    Http.send();
+    Http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           var data = JSON.parse(this.responseText);
+           console.log(data);
+        }
+    };
 
-    Http.onreadystatechange=(e)=>{
-        console.log(Http.responseText)
-    }
+    Http.open("GET", url, true);
+    Http.send();
 }
 
 function signout() {
