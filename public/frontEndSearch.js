b829d84e3a447b7cb85fb4722c0fc348df525517
Malcolm James <mtj416@gmail.com>
2018-12-14 11:45:52 -0500

    New Updates

--- public/frontEndSearch.js
+++ public/frontEndSearch.js
@@ -22,16 +22,66 @@ function scraper() {
 }
 
 function Search() {
+    console.log('In Symbol');
+
     //Get elements
     const db = firebase.database();
-    const symbols = db.child('');
-    
-   
+    var query = db.ref("Main").orderByKey();
+    //var result =[];
+
+    query.once("value").then(function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            if (childSnapshot.child('symbol').val() == request) {
+                console.log(childSnapshot);
+            }
+        });
+    });
+
+
+
 }
 
 function SearchDate() {
     //Get elements
- 
+    console.log('In Date');
+
+    var result = [];
+    var index = 0;
+    db.ref("Main").once("value", function (snap) {
+        snap.forEach(function (childSnapshot) {
+            // var key = childSnapshot.ref;
+            if (childSnapshot.hasChild("date") == true) {
+                var date_time = childSnapshot.child("date").val();
+                //console.log("\n");
+                //console.log(index + " " + date + "\n");
+                if (request == date) {
+                    response.send(childSnapshot);
+                } else {
+                    console.log("Child doesn't have date");
+                }
+            }
+
+        })
+    });
+
+    var myObj, i, j, x = " ";
+    myObj = {
+        "name": "John",
+        "age": 30,
+        "cars": [
+            { "name": "Ford", "models": ["Fiesta", "Focus", "Mustang"] },
+            { "name": "BMW", "models": ["320", "X3", "X5"] },
+            { "name": "Fiat", "models": ["500", "Panda"] }
+        ]
+    }
+
+    for (i in myObj.cars) {
+        x += "<h2>" + myObj.cars[i].name + "</h2>";
+        for (j in myObj.cars[i].models) {
+            x += myObj.cars[i].models[j] + "<br>";
+        }
+    }
+    document.getElementById("DataTable").innerHTML = x;
 }
 
 
@@ -61,73 +111,73 @@ function signout() {
     });
 }
 
-function myFunction() {
+/* function myFunction() {
 
-    var x = document.getElementById("selection").value;
-    if (x == "y") {
-        document.getElementById('symbol').style.visibility = 'visible';
-        document.getElementById('newdate').style.visibility = 'hidden';
-    }
-    else if (x == "z") {
-        document.getElementById('newdate').style.visibility = 'visible';
-        document.getElementById('symbol').style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById('symbol').style.visibility = 'hidden';
-        document.getElementById('newdate').style.visibility = 'hidden';
-    }
-}
+            var x = document.getElementById("selection").value;
+            if (x == "y") {
+                document.getElementById('symbol').style.visibility = 'visible';
+                document.getElementById('newdate').style.visibility = 'hidden';
+            }
+            else if (x == "z") {
+                document.getElementById('newdate').style.visibility = 'visible';
+                document.getElementById('symbol').style.visibility = 'hidden';
+            }
+            else {
+                document.getElementById('symbol').style.visibility = 'hidden';
+                document.getElementById('newdate').style.visibility = 'hidden';
+            }
+        }
 
 function object1() {
-    document.getElementById('exp').style.visibility = "visible";
-    document.getElementById('printtable').style.visibility = "visible";
-}
+            document.getElementById('exp').style.visibility = "visible";
+            document.getElementById('printtable').style.visibility = "visible";
+        }
 
 function downloadCsv(csv, filename) {
-    var csvFile;
-    var downloadLink;
+            var csvFile;
+            var downloadLink;
 
-    csvFile = new Blob([csv], { type: "text/csv" });
+            csvFile = new Blob([csv], { type: "text/csv" });
 
-    // download link for file
-    downloadLink = document.createElement("a");
+            // download link for file
+            downloadLink = document.createElement("a");
 
-    //filename
-    downloadLink.download = filename;
+            //filename
+            downloadLink.download = filename;
 
-    // link to file
-    downloadLink.href = window.URL.createObjectURL(csvFile);
+            // link to file
+            downloadLink.href = window.URL.createObjectURL(csvFile);
 
-    // making sure link is not displayed
-    downloadLink.style.display = "none";
+            // making sure link is not displayed
+            downloadLink.style.display = "none";
 
-    //linking to download/body
-    document.body.appendChild(downloadLink);
+            //linking to download/body
+            document.body.appendChild(downloadLink);
 
-    //onclick
-    downloadLink.click();
-}
+            //onclick
+            downloadLink.click();
+        }
 
 function exportTableToCsv(filename) {
-    var csv = [];
-    var rows = document.querySelectorAll("table tr");
+            var csv = [];
+            var rows = document.querySelectorAll("table tr");
 
-    for (var i = 0; i < rows.length; i++) {
-        var row = [], cols = rows[i].querySelectorAll("td, th");
+            for (var i = 0; i < rows.length; i++) {
+                var row = [], cols = rows[i].querySelectorAll("td, th");
 
-        for (var j = 0; j < cols.length; j++)
-            row.push(cols[j].innerText);
+                for (var j = 0; j < cols.length; j++)
+                    row.push(cols[j].innerText);
 
-        csv.push(row.join(","));
-    }
-    //downloads the csv file
-    downloadCsv(csv.join("\n"), filename);
-}
+                csv.push(row.join(","));
+            }
+            //downloads the csv file
+            downloadCsv(csv.join("\n"), filename);
+        }
 
 function printData() {
-    var divToPrint = document.getElementById("print");
-    newWin = window.open("");
-    newWin.document.write(divToPrint.outerHTML);
-    newWin.print();
-    newWin.close();
-}
+            var divToPrint = document.getElementById("print");
+            newWin = window.open("");
+            newWin.document.write(divToPrint.outerHTML);
+            newWin.print();
+            newWin.close();
+        } */
